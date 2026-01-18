7acb362e40a4ea10ac271bf2647f1eee731a2d44
PRANEETH <145996746+Praneeth1136@users.noreply.github.com>
2026-01-19 01:53:04 +0530

    Getting main video for browse page

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,22 +1,15 @@
 import Header from './Header'
-import { API_OPTIONS } from '../utils/constants'
-import { useEffect } from 'react'
+import useNowPlayingMovies from '../Hooks/useNowPlayingMovies'
+import MainContainer from './MainContainer'
+import SecondaryContainer from './SecondaryContainer'
 
 const Browse = () => {
-    const getNowPlayingMovies = async () => {
-        const data = await fetch(
-            'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1',
-            API_OPTIONS
-        )
-        const json = await data.json();
-        console.log(json.results);
-    }
-
-    useEffect(()=>{getNowPlayingMovies()},[])
+    useNowPlayingMovies()
     return (
         <div>
             <Header />
-            Browse
+            <MainContainer />
+            <SecondaryContainer />
         </div>
     )
 }
