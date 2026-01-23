2c55410ff04cef5084534d88f3528f027f86d5b0
PRANEETH <145996746+Praneeth1136@users.noreply.github.com>
2026-01-23 20:00:14 +0530

    before styling

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,4 +16,4 @@ export const API_OPTIONS = {
 }
 export const IMG_CDN_URL = 'https://image.tmdb.org/t/p/w500'
 
-export const GEMINI_API_KEY = 'AIzaSyB9TnGAVSeU0mXX5PBb0HiGX0QzXjEvnVM'
+export const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY
